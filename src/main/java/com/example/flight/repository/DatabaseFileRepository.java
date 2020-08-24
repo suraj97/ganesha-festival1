6ca8f3f6360025ed02720d864160cf8f4ede9863
suraj97 <55835075+suraj97@users.noreply.github.com>
2020-08-24 11:56:51 +0530

    Update DatabaseFileRepository.java

--- src/main/java/com/example/flight/repository/DatabaseFileRepository.java
+++ src/main/java/com/example/flight/repository/DatabaseFileRepository.java
@@ -14,6 +14,6 @@ public interface DatabaseFileRepository extends JpaRepository<DatabaseFile, Stri
 	
 	List<DatabaseFile> findByairlineName(String airlineName);
 
-	/*List<DatabaseFile> findBydetails(String details);*/
+	List<DatabaseFile> findBydetails(String airlineName);
 
 }
